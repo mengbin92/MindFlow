130be521b09e171bcfe92ff397f5bfe4334314b0
mengbin <mengbin@chengfangtech.com>
2026-04-03 11:33:05 +0800

    test(mobile): add integration test infrastructure

---
+++ packages/mobile/integration_test/app_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:mindflow/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('App Integration Tests', () {
+    testWidgets('app starts and shows workspace', (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // App should render without crashing
+      expect(find.byType(MaterialApp), findsOneWidget);
+    });
+  });
+}
